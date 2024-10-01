b66a5a965d052ec68b8246e1c16aae84d12c86c3
Coder-Sakura <1508015265@qq.com>
2024-10-01 19:20:12 +0800

    外置配置文件，日志打印处理优化

--- 0x0b-批量拖拽-文件后缀名重命名工具/filerenamer.py
+++ 0x0b-批量拖拽-文件后缀名重命名工具/filerenamer.py
@@ -3,7 +3,7 @@
 @File    :   filerenamer.py
 @Time    :   2024/09/06 21:29:04
 @Author  :   Coder-Sakura
-@Version :   1.0
+@Version :   1.1
 @Desc    :   None
 '''
 
@@ -11,20 +11,26 @@
 import wx
 import os
 import re
+import json
 from datetime import datetime
 from loguru import logger
 
 
 # 配置loguru日志
-logger.add("{time}.log",encoding="utf-8",enqueue=True)
+logger.add("{time:YYYY-MM}.log",encoding="utf-8",enqueue=True)
 # 跳过名单 , "."
-skip_formats = {".mp4", ".avi", ".mov", ".mkv", ".flv", ".zip", ".rar", ".7z", ".tar.gz", ".EXE", ".exe",\
+skip_formats = [".mp4", ".avi", ".mov", ".mkv", ".flv", ".zip", ".rar", ".7z", ".tar.gz", ".EXE", ".exe",\
                 ".png", ".jpg", ".jpeg", ".gif", ".ico", ".psd", ".webp", ".mp3", ".wav", ".ass", ".lrc",\
-                ".vtt", ".WAV", ".html"}
-# 检测名单
-target_formats = {".zi", ".rr"}
+                ".vtt", ".WAV", ".html"]
+# 检测名单 , ""
+target_formats = [".zi", ".rr", ".r", ".ra"]
 # 无后缀名时默认添加的后缀名
 default_suffix = ".zip"
+configName = "config.json"
+if not os.path.exists(configName):
+    with open(configName, "w", encoding="utf-8") as f:
+        json.dump({"skip_formats":skip_formats, "target_formats":target_formats, "default_suffix":default_suffix},\
+                   f, ensure_ascii=False, indent=4)
 
 
 class FileDrop(wx.FileDropTarget):
@@ -64,9 +70,10 @@ class DropFile(wx.Frame):
     def __init__(self, *args, **kw):
         wx.Frame.__init__(self, *args, **kw)
         self.InitUI()
+        self.InitConfig()
 
     def InitUI(self):
-        self.title = "文件重命名工具"
+        self.title = "文件重命名工具 By Coder-Sakura"
 
         # 启用拖放
         self.text = wx.TextCtrl(self, -1, style = wx.TE_MULTILINE | wx.TE_RICH)
@@ -80,7 +87,14 @@ class DropFile(wx.Frame):
         self.SetTitle(self.title)
         self.Show(True)
 
-    def WriteLog(self, text, color=""):
+    def InitConfig(self):
+        with open(configName, "r", encoding="utf-8") as f:
+            self.configData = json.load(f)
+            self.skip_formats = self.configData["skip_formats"]
+            self.target_formats = self.configData["target_formats"]
+            self.default_suffix = self.configData["default_suffix"]
+
+    def WriteLog(self, text, level="info", color=""):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         start = self.text.GetInsertionPoint()   # 获取当前文本位置
         attr = wx.TextAttr()
@@ -88,12 +102,13 @@ class DropFile(wx.Frame):
         attr.SetTextColour([wx.Colour(255, 0, 0) if color == "red" else wx.Colour(0, 0, 255) if color == "blue" else wx.Colour(0, 0, 0)][0])  # 红/蓝/黑
         self.text.WriteText(f"{now} {text}\n")
         self.text.SetStyle(start, start + len(now+text), attr)  # 插入文本并应用样式
+        
+        log_level_method = getattr(logger, level, None)
+        log_level_method(f"{now} {text}")
 
     def RenameFile(self, file_path):
         dir,fullfname = os.path.split(file_path)
         fname,ext = os.path.splitext(fullfname)
-        # logger.info(f"检测到文件 - {dir}|{fullfname}|{fname}|{ext}")
-        # return
 
         # ================= 后缀名处理 ================= #
         # 跳过名单的后缀名 -> 跳过
@@ -105,8 +120,7 @@ class DropFile(wx.Frame):
         if not ext and fullfname == fname:
             dst = os.path.join(dir, f"{fname}{default_suffix}")
             os.rename(file_path, dst)
-            self.WriteLog(f"添加后缀名 - {dst}\n", color="blue")
-            logger.success(f"添加后缀名 - {dst}")
+            self.WriteLog(f"添加后缀名 - {dst}\n", level="success", color="blue")
             return 
         
         # 全中文后缀名/检测名单的后缀名 -> 修改为默认后缀名
@@ -114,8 +128,7 @@ class DropFile(wx.Frame):
             or ext in target_formats:
             dst = os.path.join(dir, f"{fname}{default_suffix}")
             os.rename(file_path, dst)
-            self.WriteLog(f"重命名文件 - {dst}\n", color="blue")
-            logger.success(f"重命名文件 - {dst}")
+            self.WriteLog(f"重命名文件 - {dst}\n", level="success", color="blue")
             return 
 
         # 部分中文后缀名 -> 去除中文
@@ -123,12 +136,10 @@ class DropFile(wx.Frame):
             new_ext = re.sub(r"[\u4e00-\u9fa5]", "", ext)
             dst = os.path.join(dir, f"{fname}{new_ext}")
             os.rename(file_path, dst)
-            self.WriteLog(f"重命名文件 - {dst}\n", color="blue")
-            logger.success(f"重命名文件 - {dst}")
+            self.WriteLog(f"重命名文件 - {dst}\n", level="success", color="blue")
             return 
 
-        self.WriteLog(f"未知类型 - {file_path}\n", color="red")
-        logger.warning(f"未知类型 - {file_path}")
+        self.WriteLog(f"未知类型 - {file_path}\n", level="warning", color="red")
         return
 
 
